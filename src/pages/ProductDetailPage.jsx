2c61d3b09f0f682b1f56de78b964721ae8a51d9c
mdhazar <m.derya.hazar@gmail.com>
2024-10-31 12:09:49 +0300

    Fix vercel

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -3,7 +3,7 @@ import Header from "../layouts/header";
 import Navbar from "../layouts/navbar";
 import Footer from "../layouts/footer";
 import ProductCards from "../components/ui/ProductCards";
-import ProductCardInfo from "../components/ui/productCardInfo";
+import ProductCardInfo from "../components/ui/ProductCardInfo";
 import BestSellerProducts from "../components/ui/BestSellerProducts";
 import ShopPageBrandIcons from "../components/ui/ShopPageBrandIcons";
 
