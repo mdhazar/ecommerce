95c5df85c3d1e956756ded5963207eb7f8ad9f99
mdhazar <m.derya.hazar@gmail.com>
2025-05-22 23:48:16 +0300

    Fix vercel

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import ContactSection1 from "@/components/ui/ContactPage/contactSection1";
-import ContactSection2 from "@/components/ui/ContactPage/contactSection2";
-import ContactSection3 from "@/components/ui/ContactPage/contactSection3";
-import ContactSection4 from "@/components/ui/ContactPage/contactSection4";
+import ContactSection1 from "@/components/ui/ContactPage/ContactSection1";
+import ContactSection2 from "@/components/ui/ContactPage/ContactSection2";
+import ContactSection3 from "@/components/ui/ContactPage/ContactSection3";
+import ContactSection4 from "@/components/ui/ContactPage/ContactSection4";
 
 function ContactPage() {
   return (
